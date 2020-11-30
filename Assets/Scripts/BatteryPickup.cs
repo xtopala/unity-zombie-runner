c1437ddfe102d8b9848de3137639a22843135ee7
xtopala <xtopala@gmail.com>
2020-11-30 18:12:28 +0100

    Created a battery pickup

---
+++ Assets/Scripts/BatteryPickup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BatteryPickup : MonoBehaviour
+{
+    [SerializeField] float restoreAngle = 90f;
+    [SerializeField] float addIntensity = 1f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.GetComponentInChildren<FlashLightSystem>().RestoreLightAngle(restoreAngle);
+            other.GetComponentInChildren<FlashLightSystem>().AddLightIntensity(addIntensity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/FlashLightSystem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+public class FlashLightSystem : MonoBehaviour
+{
+    [SerializeField] float lightDecay = 0.1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minAngle = 40f;
+
+    Light myLight;
+
+    private void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreseLightAngle();
+        DecreseLightIntensity();
+    }
+
+    public void RestoreLightAngle(float restoreAngle)
+    {
+        myLight.spotAngle = restoreAngle;
+    }
+
+    public void AddLightIntensity(float intensityAmount)
+    {
+        myLight.intensity += intensityAmount;
+    }
+
+    private void DecreseLightAngle()
+    {
+        if (myLight.spotAngle <= minAngle)
+        {
+            return;
+        }
+        else
+        {
+            myLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+    }
+
+    private void DecreseLightIntensity()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+}
